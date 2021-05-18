5eedc92b2a679b66bfbdf0fbb2552a64b946c375
Olex1313 <aalimonov_2@miem.hse.ru>
2021-05-18 20:07:52 +0300

    some architect

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,32 @@
+from flask.signals import request_started
 from app import app
-from flask import render_template
+from flask import render_template, request
 import requests
 import apimoex
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def parse_json(json):
+    pass
+
+def get_data(params):
+    pass
+
+def receive_ML(X, Y):
+    pass
+
+def make_graph(data):
+    pass
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
-@app.route('/info/<ticket>')
-def info(ticket):
-    with requests.Session() as session:
-        data = apimoex.get_market_candles(session, ticket, interval = 24, start = '2021-04-01', end = '2021-05-05')
-        df = pd.DataFrame(data)
-        return render_template('info.html', df=df.items())
+@app.route('/plot/past', methods=['GET', 'POST'])
+def plot_past_view(ticket):
+    params = parse_json(request.json)
+    X, Y = get_data(params)
+    X, Y = receive_ML(X, Y)
+    plotly_graph = make_graph(X, Y)
+    return plotly_graph
+    
