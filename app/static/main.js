7ee38a79446a2cfc929ddd2f36f52929b44e1cad
Zergey Loshkarev <zeryoshka@gmail.com>
2021-05-24 00:13:02 +0300

    wrote script for request plot/past

--- app/static/main.js
+++ app/static/main.js
@@ -1,1 +1,28 @@
 Plotly.plot('chart', {}, {})
+
+function create_data_obj() {
+    let models = []
+    $('.select-models__checkbox').each(function(index, checkbox) {
+        if ($(checkbox).is(':checked'))
+            models.push($(checkbox).attr('name'))
+    })
+
+    let ans = {
+        ticket: $('#ticket-select').val(),
+        models: models
+    }
+    return ans;
+}
+
+$('.form__submit').click(function (){
+    $.ajax({
+        url: '/plot/past',
+        method: 'get',
+        dataType: 'json',
+        data: create_data_obj(),
+        success: function(data) {
+            Plotly.plot('chart', data.graph, {})
+        }
+    })
+    return false
+});
