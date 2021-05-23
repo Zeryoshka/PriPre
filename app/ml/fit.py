26c4b73ccc908d70689939aca2b94c297bf4ef6e
Zergey Loshkarev <zeryoshka@gmail.com>
2021-05-23 12:04:52 +0300

    added test-script to check work const_model

---
+++ app/ml/fit.py
@@ -0,0 +1,6 @@
+# from const_model
+from const_model import Const
+
+model = Const()
+model.fit()
+model.dump()
---
+++ app/ml/test_predict.py
@@ -0,0 +1,5 @@
+from const_model import Const
+
+model = Const()
+model.load()
+print(model.predict([0,1,2,3], [0,1,2,3], [0,1,2,3]))
