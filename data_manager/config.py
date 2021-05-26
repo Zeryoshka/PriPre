1bdb1b424349c1f84b075576852d91ac723f0caa
Olex1313 <aalimonov_2@miem.hse.ru>
2021-05-26 14:48:27 +0300

    PEP8 Fixes

--- data_manager/config.py
+++ data_manager/config.py
@@ -1,3 +1,3 @@
-START_DATE = '2021-01-01'
-SECURITY_LIST = ['YNDX', 'ALRS', 'SBER', 'MOEX']
-DATA_PATH = 'app/loaded_data/'
+START_DATE = '2021-01-01'  # Tickets loads from this date
+SECURITY_LIST = ['YNDX', 'ALRS', 'SBER', 'MOEX']  # Ticket for download list
+DATA_PATH = 'app/loaded_data/'  # Path, where csv is created and stored
