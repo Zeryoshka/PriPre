f20b2af557f04e8e29c10b1c28acdc5950879522
Olex1313 <aalimonov_2@miem.hse.ru>
2021-05-26 14:58:47 +0300

    Add commentary

--- data_manager/config.py
+++ data_manager/config.py
@@ -1,3 +1,4 @@
-START_DATE = '2021-01-01'
+START_DATE = '2021-01-01'  # Дата с которой выгружаются данные, формат YYYY-MM-DD
+# Общий список тиктов на выгрузку
 SECURITY_LIST = ['YNDX', 'ALRS', 'SBER', 'MOEX']
-DATA_PATH = 'app/loaded_data/'
+DATA_PATH = 'app/loaded_data/'  # Путь к директории для выгрузки
