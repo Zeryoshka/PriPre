b4fcd9e9a3f76cce2c70bfe1b94ed904f4e6be20
Olex1313 <aalimonov_2@miem.hse.ru>
2021-05-26 15:06:24 +0300

    fix git

--- data_manager/config.py
+++ data_manager/config.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 START_DATE = '2021-01-01'  # Дата с которой выгружаются данные, формат YYYY-MM-DD
 # Общий список тиктов на выгрузку
 SECURITY_LIST = ['YNDX', 'ALRS', 'SBER', 'MOEX']
 DATA_PATH = 'app/loaded_data/'  # Путь к директории для выгрузки
-=======
-START_DATE = '2021-01-01'  # Tickets loads from this date
-SECURITY_LIST = ['YNDX', 'ALRS', 'SBER', 'MOEX']  # Ticket for download list
-DATA_PATH = 'app/loaded_data/'  # Path, where csv is created and stored
->>>>>>> 65a99687f6e51fdd639fb73c1f0c55f2cb6c5655
