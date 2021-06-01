0e7b4dd58330e08cbf2da19193ac68d89dfb5d3d
Olex1313 <aalimonov_2@miem.hse.ru>
2021-06-01 13:46:33 +0300

    Lint fix with Black linter

--- app/routes.py
+++ app/routes.py
@@ -11,49 +11,38 @@ import plotly.graph_objects as go
 import json
 
 
-@app.route('/')
+@app.route("/")
 def index():
     # Заглушка, чтобы было проще понимать какие параметры требуются для рендера
     parametrs = {
         # Наимаенования тикетов (списком строк)
-        'tickets': data_manager.ticket_list,
-        'models': models.names  # Наименования моделей (списком строк)
+        "tickets": data_manager.ticket_list,
+        "models": models.names,  # Наименования моделей (списком строк)
     }
     # !Внимательнее там **parametrs
-    return render_template('index-template.html', **parametrs)
+    return render_template("index-template.html", **parametrs)
 
 
-@app.route('/plot/past', methods=['POST'])
+@app.route("/plot/past", methods=["POST"])
 def plot_past_view():
     # dict with to param ticket(str) and list of names's strings, named model
     params = request.get_json()
-    ticket = params['ticket']  # Ticket name from client
-    model_list = params['models']  # Models list : str
+    ticket = params["ticket"]  # Ticket name from client
+    model_list = params["models"]  # Models list : str
 
     X, Y = data_manager.give_data(ticket)
     fig = go.Figure()
-    fig.add_trace(
-        go.Scatter(
-            x=X,
-            x0=X[0],
-            y=Y,
-            y0=Y[0],
-            name='Real value'
-        )
-    )
+    fig.add_trace(go.Scatter(x=X, x0=X[0], y=Y, y0=Y[0], name="Real value"))
     fig.update_layout(
-        title=go.layout.Title(text=f'PriPre {ticket} ticket graph'),
-        yaxis_title='Close value',
-        xaxis_title='Date',
+        title=go.layout.Title(text=f"PriPre {ticket} ticket graph"),
+        yaxis_title="Close value",
+        xaxis_title="Date",
         showlegend=True,
-        legend_title_text='Tickets',
-        font=dict(
-            family="Courier New, monospace",
-            size=18,
-            color="Black"
-        )
+        legend_title_text="Tickets",
+        font=dict(family="Courier New, monospace", size=18, color="Black"),
     )
-
-    
-    return json.dumps(fig, cls=PlotlyJSONEncoder),\
-        200, {'Content-Type': 'application/json'}
+    return (
+        json.dumps(fig, cls=PlotlyJSONEncoder),
+        200,
+        {"Content-Type": "application/json"},
+    )
--- data_manager/config.py
+++ data_manager/config.py
@@ -1,5 +1,5 @@
-START_DATE = '2021-01-01'  # Дата с которой выгружаются данные, формат YYYY-MM-DD
+START_DATE = "2021-01-01"  # Дата начала, формат YYYY-MM-DD
 # Общий список тиктов на выгрузку
-SECURITY_LIST = ['YNDX', 'ALRS', 'SBER', 'MOEX']
-DATA_PATH = 'app/loaded_data/'  # Путь к директории для выгрузки
-INTERVAL = 10  # Интервал для выгрузки данных, 1 = 1 мин, 10 = 10 мин, 60 = 1 час, 24 = 1 день
+SECURITY_LIST = ["YNDX", "ALRS", "SBER", "MOEX"]
+DATA_PATH = "app/loaded_data/"  # Путь к директории для выгрузки
+INTERVAL = 10  # Интервал, 1 = 1 мин, 10 = 10 мин, 60 = 1 час, 24 = 1 день
