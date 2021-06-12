717e315120e210afafd2422f1a34d19a55dc7bca
Zergey Loshkarev <zeryoshka@gmail.com>
2021-06-12 13:12:26 +0300

    delete import tensorflow

--- app/app.py
+++ app/app.py
@@ -4,9 +4,20 @@ File to include all needed imports
 
 from flask import Flask
 from data_manager import DataManager
-from ml import Models
-from ml import MODELS
+# from ml import Models
+# from ml import MODELS
+
+class Models():
+    """
+    cover
+    """
+    def __init__(self):
+        """
+        cover
+        """
+        self.names =  ["It is alive"]
+
+models = Models()
 
 app = Flask(__name__)
 data_manager = DataManager()
-models = Models(MODELS)
